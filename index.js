2e3182b79bf418458df8fb634e46c8b54ea44db7
Matthew McIvor <matthewrmcivor@gmail.com>
2025-01-07 19:34:49 -0800

    square blank maze

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-  MouseConstraint,
-  Mouse
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const width = 600;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -14,27 +9,19 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: true,
+    width,
+    height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-  })
-);
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
-
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
